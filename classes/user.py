26c8865fa7b0c2cf4ea20234b7ced768ffb0d3e1
JanikCodes <piefke2000@gmx.de>
2023-03-26 14:09:30 +0200

    Fixed constructor

--- classes/user.py
+++ classes/user.py
@@ -1,9 +1,8 @@
 import database
 class User:
     def __init__(self, userID = None):
-        if not userID:
+        if userID:
             self.gradeList = database.get_grade_list_from_userID(userID)
-
 
     def get_gradeList(self):
         return self.gradeList
