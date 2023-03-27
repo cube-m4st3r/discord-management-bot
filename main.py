609c0a7ffc140683d7b2afd384a8c70630a72239
cube-m4st3r <benjaminfinck02@googlemail.com>
2023-03-27 23:24:27 +0200

    Created a change_presence task.loop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import platform
+import random
 import time
 
 import discord
@@ -10,7 +11,14 @@ from colorama import Back, Fore, Style
 
 load_dotenv("settings.env")
 
-my_guild = discord.Object(id=1076193627778326671)
+my_guild = discord.Object(id=os.getenv("GUILD-ID"))
+
+@tasks.loop(minutes=5.0)
+async def update_presence(self):
+    presence_game_list = ['with Documents', 'with Papers', 'with Books']
+
+    random_presence = random.choice(presence_game_list)
+    await self.change_presence(status=discord.Status.dnd, activity=discord.Game(name=random_presence))
 
 class Client(commands.Bot):
     def __init__(self):
@@ -30,14 +38,15 @@ class Client(commands.Bot):
     async def on_ready(self):
         prfx = (Back.BLACK + Fore.GREEN + time.strftime("%H:%M:%S UTC", time.gmtime()) + Back.RESET + Fore.WHITE + Style.BRIGHT)
         print(f"{prfx} Logged in as {Fore.YELLOW + self.user.name}")
-        await client.change_presence(status=discord.Status.dnd, activity=discord.Game(name="with Documents"))
         print(f"{prfx} Bot ID: {Fore.YELLOW + str(self.user.id)}")
         print(f"{prfx} Discord Version: {Fore.YELLOW + discord.__version__}")
         print(f"{prfx} Python Version: {Fore.YELLOW + str(platform.python_version())}")
         self.tree.copy_global_to(guild=discord.Object(id=1076193627778326671))
         print(f"{prfx} Slash CMDs Synced: {Fore.YELLOW + str(len(await self.tree.fetch_commands(guild=my_guild)))} Commands")
         print(f"{prfx} Connected to: {Fore.YELLOW + str(len(self.guilds))} Guilds")
-        await database.init_database()
+        if await database.init_database():
+            print(f"{prfx} Database Connection:{Fore.YELLOW} successful.")
+        await update_presence.start(self)
 
 client = Client()
 client.run(os.getenv("TOKEN"))
