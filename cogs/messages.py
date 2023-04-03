6aa963324b3a26cf74cee3f63e093d0ef7fa182b
cube-m4st3r <benjaminfinck02@googlemail.com>
2023-04-04 00:09:00 +0200

    cleaned imports

--- cogs/messages.py
+++ cogs/messages.py
@@ -2,9 +2,6 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-import mysql.connector
-import os
-
 class Messages(commands.Cog):
 
     def __init__(self, bot):
